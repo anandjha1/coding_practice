783846e4d6e6ba6fa27e3e986e30f00618b33ebc
ANAND KUMAR JHA <delhi.akjha@gmail.com>
2023-11-05 02:24:48 +0530

    Create Count Digits.cpp

---
+++ Count Digits.cpp
@@ -0,0 +1,16 @@
+// find the number of digit 'n' that evenly divide 'n'
+
+int countDigits(int n){
+	// Write your code here.
+	int count = 0;
+	int num = n;
+	while(n>0){
+		int digit = n%10;
+		if(digit && num%digit == 0){
+			count++;
+		}
+		n = n/10;
+	}
+
+	return count;	
+}
